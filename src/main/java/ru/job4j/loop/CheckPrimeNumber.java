e2f122c3b7833438cfb3f2c9544de07f1377dc96
mikhail <sandreyf1972@gmail.com>
2024-11-16 00:41:28 +0600

    5.6. Простые числа [#505215]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         int hold = 0;
-        for (int index = 1; index <= number; index++) {
+        for (int index = 2; index <= number; index++) {
             if (number % index == 0) {
                 hold++;
             }
@@ -11,12 +11,13 @@ public class CheckPrimeNumber {
                 break;
             }
         }
-        return hold <= 2;
+        return hold <= 2 && number != 4;
     }
 
     public static void main(String[] args) {
         System.out.println(check(6));
         System.out.println(check(5));
         System.out.println(check(3));
+        System.out.println(check(4));
     }
 }
---
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class PrimeNumberTest {
+
+    @Test
+    void whenFinish5Then3() {
+        int finish = 5;
+        int expected = 3;
+        int output = PrimeNumber.calc(finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFinish15Then7() {
+        int finish = 15;
+        int expected = 7;
+        int output = PrimeNumber.calc(finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFinish0Then0() {
+        int finish = 0;
+        int expected = 0;
+        int output = PrimeNumber.calc(finish);
+        assertThat(output).isEqualTo(expected);
+    }
+}
