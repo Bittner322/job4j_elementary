762a4c590a8261c78e6eba7ad8348be502c8e681
mikhail <sandreyf1972@gmail.com>
2024-11-17 19:54:33 +0600

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Михаил";
+        names[1] = "Петр";
+        names[2] = "Стас";
+        names[3] = "Игорь";
+
+        for (String name : names) {
+            System.out.println(name);
+        }
 
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
